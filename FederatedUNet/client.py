c70b4304b2dda894793f87fd5e5a64ef8f4d4785
alexander <a.samoyl11@gmail.com>
2020-05-09 18:29:07 +0300

    multiple clients init

--- FederatedUNet/client.py
+++ FederatedUNet/client.py
@@ -2,21 +2,32 @@ from model.model import UNet
 import torch
 import socket
 import pickle
-
+import time
 model = UNet(n_channels=1, n_classes=1).float()
+
+start = time.time()
 sock = socket.socket()
-sock.connect(('neuro-x5', 9091))
+sock.connect(('neuro-t', 9091))
 
 data = b""
 
 while True:
-    packet = sock.recv(65536)
+    packet = sock.recv(65536 * 2)
     if not packet:
         break
     data += packet
 sock.close()
-
+print('received all')
+print(time.time() - start)
 weights = pickle.loads(data)
 model.load_state_dict(weights)
 
-print(model)
+print('DOING SOMETHING')
+time.sleep(30)
+
+sock = socket.socket()
+sock.connect(('neuro-t', 9091))
+sock.send(pickle.dumps(weights))
+sock.close()
+
+
